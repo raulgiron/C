78c70b5fcdc7140558ddbf296cc3ff6f3c537262
Raul Giron (he/him/his) <raulgiron@hotmail.com>
2022-01-30 14:59:43 -0400

    Conver Meter to Feet.

---
+++ Meter_to_Feet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+  double conv, initial = 0, step = 0, stop = 0;
+
+  do
+  {
+    printf("Initial (m): ");
+    scanf("%lf", &initial);
+    if (initial < 0) printf("Inital must be >=0!\n");
+  } while (initial < 0);  
+  
+  do
+  {
+    printf("Step (m): ");
+    scanf("%lf", &step);
+    if (step <= 0) printf("Step must be >0!\n");
+  } while (step <= 0);
+  
+  do
+  {
+    printf("Stop (m): ");
+    scanf("%lf", &stop);
+    if (stop < 0) printf("Stop must be >=0!\n");
+  } while (stop < 0);
+
+  printf("\n");
+  printf("Meters    Feet\n");
+  printf("********************\n");
+
+  for (conv = initial; conv <= stop; conv += step)
+    printf("%-10.2lf %-10.2lf\n", conv, conv * 3.28084);
+
+  printf("\n");
+
+  return 0;
+}
